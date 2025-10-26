1e1cbe876e4f2c3a49b5f5c2a69079671e9c3256
GaneshGupta3 <ganesh310104@gmail.com>
2025-10-27 01:22:27 +0530

    added the feature as requested in issue

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const qrText = document.getElementById('qr-text');
 const sizes = document.getElementById('sizes');
 const generateBtn = document.getElementById('generateBtn');
+const downloadBtn = document.getElementById('downloadBtn');
 const qrContainer = document.querySelector('.qr-body');
 
 let size = sizes.value;
@@ -14,7 +15,17 @@ sizes.addEventListener('change',(e)=>{
     isEmptyInput();
 });
 
+downloadBtn.addEventListener('click', ()=>{
+    let img = document.querySelector('.qr-body img');
 
+    if(img !== null){
+        let imgAtrr = img.getAttribute('src');
+        downloadBtn.setAttribute("href", imgAtrr);
+    }
+    else{
+        downloadBtn.setAttribute("href", `${document.querySelector('canvas').toDataURL()}`);
+    }
+});
 
 function isEmptyInput(){
     // if(qrText.value.length > 0){
